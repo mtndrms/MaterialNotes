8a4a9b5397d3b3d4521acb780b15f9f4f2663f6e
Metin Durmus <mtn.drms@outlook.com>
2022-08-13 17:31:53 +0300

    initial commit

---
+++ app/src/main/java/com/materialtools/notes/activities/MainActivity.kt
@@ -0,0 +1,12 @@
+package com.materialtools.notes.activities
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.materialtools.notes.R
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
